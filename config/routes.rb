ee5fed7e5b1b24f1d8769d9953884588ba2c9cf5
Honor Lin <honor.lin@jcmanagement.com.tw>
2014-10-23 15:38:19 +0800

    group controller and group index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Iorder::Application.routes.draw do
  
+
   devise_for :users
   resources :shops
+  resources :groups
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
